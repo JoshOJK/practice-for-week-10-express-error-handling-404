7074422328bd8c4406b817f23f16ffde4c3978a8
joshjk <jokrienke@comcast.net>
2023-04-26 21:13:48 -0700

    completed

--- server/app.js
+++ server/app.js
@@ -5,5 +5,24 @@ app.get('/', (req, res) => {
   res.send('GET / This is the root URL');
 });
 
+app.use((req, res, next) => {
+  const err = new Error("Something fucked up")
+  err.statusCode = 404;
+  next(err);
+})
+
+app.use((err, req, res, next) => {
+  console.log(err)
+  res.statusCode = err.statusCode || 500
+  const response = {
+    message: err.message || 'Something went wrong',
+    statusCode: err.statusCode || 500,
+
+
+  }
+
+  res.json(response)
+})
+
 const port = 5000;
 app.listen(port, () => console.log('Server is listening on port', port));
